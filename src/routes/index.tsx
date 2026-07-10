5af71d7cd6dc252797a8844b62326de5bd5023fe
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-10 15:01:40 -0300

    umcasaldeamigosmeu

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { motion } from "framer-motion";
 import { ArrowUpRight } from "lucide-react";
-import heroImg from "@/assets/main_picture/hero-construction.jpg";
+import heroImg from "@/assets/main_picture/channels4_profile.jpg";
 
 // Rota adicionada explicitamente para resolver o erro do TS
 export const Route = createFileRoute("/")({
